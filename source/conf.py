c7f54e5bd5dca648fd73391b4dd48037afbdfdfc
Jacklyn <jacklynlee31@gmail.com>
2018-07-24 08:47:11 -0500

    Added new rst files to html_sidebars

--- source/conf.py
+++ source/conf.py
@@ -97,7 +97,15 @@ html_static_path = ['_static']
 # default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
 # 'searchbox.html']``.
 #
-# html_sidebars = {}
+html_sidebars = {
+    '**': [
+        'start.rst'
+        'installmac.rst'
+        'installwin.rst'
+        'using.rst'
+        'wrapup.rst'
+    ]
+}
 
 
 # -- Options for HTMLHelp output ---------------------------------------------
